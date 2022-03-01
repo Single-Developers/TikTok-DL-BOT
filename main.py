8fd09a358aa6a377d1ff4ac7145b471e1fbfd37d
Malith Rukshan <85282650+Malith-Rukshan@users.noreply.github.com>
2022-03-01 15:09:59 +0530

    🔥 Source Code - API Domain Updated
    single-developers.up.railway.app

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import logging
 
 
 # TikTok Downloader API
-API = 'https://api.single-developers.software/tiktok?url='
+API = 'https://single-developers.up.railway.app/tiktok?url='
 
 # Your BOT Token
 BOT_TOKEN = os.getenv("BOT_TOKEN")
